65124ca1177adb75a069b0be0afa1ba1e7ef6299
Isaac Vergara <isaacvergara99@gmail.com>
2023-09-10 13:37:03 +0200

    Update random provider property

--- infraestructure/main.tf
+++ infraestructure/main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_resource_group" "rg-azurecloudresume" {
-  name = join("-", ["rg", var.projectname])
+  name     = join("-", ["rg", var.projectname])
   location = var.location
 }
 
 # Storage account name
 resource "random_string" "sa-name" {
-  length = 24
+  length  = 24
   special = false
-  upper = false
+  upper   = false
 }
 
 resource "azurerm_storage_account" "sa-azurecloudresume" {
-  name = random_string.sa-name
-  resource_group_name = azurerm_resource_group.rg-azurecloudresume.name
-  location = azurerm_resource_group.rg-azurecloudresume.location
-  account_tier = "Standard"
+  name                     = random_string.sa-name.result
+  resource_group_name      = azurerm_resource_group.rg-azurecloudresume.name
+  location                 = azurerm_resource_group.rg-azurecloudresume.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
   # Static website. $web container wil be created, load site on that container
@@ -24,6 +24,6 @@ resource "azurerm_storage_account" "sa-azurecloudresume" {
 
   tags = {
     projectname = var.projectname
-    stage = "Production"
+    stage       = "Production"
   }
 }
