560cb97751c4ac1b88463a975ecdf51f6c840d8e
Isaac Vergara <isaacvergara99@gmail.com>
2023-09-23 17:03:23 +0200

    Add delivery rule to CDN

--- infraestructure/modules/cdn/main.tf
+++ infraestructure/modules/cdn/main.tf
@@ -20,6 +20,22 @@ resource "azurerm_cdn_endpoint" "resume_endpoint" {
     name = "static-website-origin"
     host_name = var.endpoint.static_site_endpoint
   }
+  origin_host_header = var.endpoint.static_site_endpoint
+  delivery_rule {
+    name = "EnforceHTTPS"
+    order = 1
+    request_scheme_condition {
+      match_values = [
+        "HTTP"
+      ]
+      negate_condition = false
+      operator = "Equal"
+    }
+    url_redirect_action {
+      protocol = "Https"
+      redirect_type = "Found"
+    }
+  }
 }
 resource "azurerm_cdn_endpoint_custom_domain" "resume" {
     name = local.endpoint_name
