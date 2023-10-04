f8c670fe4d5e3198c6ca5e1531d68cca262288fe
Isaac Vergara <isaacvergara99@gmail.com>
2023-10-04 18:24:58 +0200

    Update local URL for JS tests

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const  localFunctionAPI = "http://localhost:8002/api/update_counter";
-const functionAPI = ""
+const  localFunctionAPI = "http://localhost:8001/api/update_counter";
+const functionAPI = "https://fapp-linux-cloudresume.azurewebsites.net/api/update_counter?code=mQccAxuYaKDSD6hIutBt_7J18Dx9-e7ekj422lJGyvHkAzFuwQG2wA=="
+
 
 const getVisitCount = () => {
     fetch(localFunctionAPI).then(response => {
